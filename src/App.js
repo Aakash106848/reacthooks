a76989bf93e5f29f476576de2ebd0a1b0ac5df16
Aakash106848 <aakash.b@gavstech.com>
2023-10-30 10:33:42 +0530

    Project Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [visitorCount, setVisitorCount] = useState(() => {
+    const storedVisitorCount = JSON.parse(localStorage.getItem("visitorCount"));
+    return storedVisitorCount ? parseInt(storedVisitorCount) : 0;
+  });
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      setVisitorCount((prevCount) => prevCount - 1);
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("visitorCount", visitorCount);
+    console.log("Visitor Count Updated" + visitorCount);
+  }, [visitorCount]);
+
+  const updateVisitorCount = () => {
+    setVisitorCount(visitorCount + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <fieldset>
+        <form>
+          <b>Your current visitor count is:</b>
+          <br />
+          <input
+            type="button"
+            onClick={updateVisitorCount}
+            value="Update Visitor"
+          />
+          <br />
+          {visitorCount}
+        </form>
+      </fieldset>
     </div>
   );
 }
